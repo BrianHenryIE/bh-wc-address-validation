537f20e74af4d4c0cbce59fab6fd2eb5ea63d2c8
Brian Henry <BrianHenryIE@gmail.com>
2023-06-01 20:27:33 -0700

    Update class-usps-address-validator-integration-Test.php

--- tests/integration/API/Validator/class-usps-address-validator-integration-Test.php
+++ tests/integration/API/Validator/class-usps-address-validator-integration-Test.php
@@ -30,6 +30,10 @@ class USPS_Address_Validator_Integration_Test extends \Codeception\TestCase\WPTe
 	 */
 	public function test_bad_addresses( array $address_array ) {
 
+		if ( ! isset( $_ENV['USPS_USERNAME'] ) ) {
+			$this->markTestIncomplete( 'Needs USPS_USERNAME in .env.secret file.' );
+		}
+
 		$usps_username = $_ENV['USPS_USERNAME'];
 
 		$logger = new ColorLogger();
@@ -52,22 +56,113 @@ class USPS_Address_Validator_Integration_Test extends \Codeception\TestCase\WPTe
 
 		$addresses = array(
 			array(
-				// Adding # or APT fixes this one.
+				// Removing apostrophe fixes this one... no it didn't. (anon).
 				array(
-					'address_1' => '4640 White Plains Road',
-					'address_2' => '3S',
-					'city'      => 'Bronx',
-					'state'     => 'NY',
-					'postcode'  => '10470',
-					'country'   => 'US',
-				),
-				// Removing apostrophe fixes this one.
-				array(
-					'address_1' => '48 Taylor\'s Pond Drive',
+					'address_1' => '60 Taylor\'s Pond Drive',
 					'address_2' => '',
 					'city'      => 'Cary',
 					'state'     => 'NC',
 					'postcode'  => '27513',
+					'country'   => 'US',
+				),
+			),
+			array(
+				// Military address cannot be validated so assume correct. (anon).
+				array(
+					'address_1' => 'Air Defense Company Z',
+					'address_2' => 'Unit 12345',
+					'city'      => 'FPO',
+					'state'     => 'AP',
+					'postcode'  => '96372',
+					'country'   => 'US',
+				),
+			),
+			array(
+				// Missing space after E. (anon).
+				array(
+					'address_1' => '556 E.22nd st',
+					'address_2' => 'Apt 1',
+					'city'      => 'Ogden',
+					'state'     => 'UT',
+					'postcode'  => '94401',
+					'country'   => 'US',
+				),
+			),
+			array(
+				// Number must come after the apartment.
+				array(
+					'address_1' => '4611 Brown street',
+					'address_2' => '1 Apt',
+					'city'      => 'Union City',
+					'state'     => 'NJ',
+					'postcode'  => '07087',
+					'country'   => 'US',
+				),
+			),
+			array(
+
+				array(
+					'address_1' => '3614 US 74',
+					'address_2' => 'Suite I',
+					'city'      => 'Wingate',
+					'state'     => 'NC',
+					'postcode'  => '28176',
+					'country'   => 'US',
+				),
+			),
+			array(
+				// Passes with no space between the W/N part. (anon)
+				array(
+					'address_1' => 'W159 N4940 Graysland Dr',
+					'address_2' => '',
+					'city'      => 'menomnee falls',
+					'state'     => 'WI',
+					'postcode'  => '53051',
+					'country'   => 'US',
+				),
+			),
+			array(
+				// Accidental space in street number. (anon)
+				array(
+					'address_1' => '13 12 63rd St',
+					'address_2' => '',
+					'city'      => 'Sacramento',
+					'state'     => 'CA',
+					'postcode'  => '95819',
+					'country'   => 'US',
+				),
+			),
+			array(
+				// Replace Interstate with I and it passes.
+				array(
+					'address_1' => '4800 East Interstate 240 Service Rd',
+					'address_2' => '',
+					'city'      => 'Oklahoma City',
+					'state'     => 'OK',
+					'postcode'  => '73135',
+					'country'   => 'US',
+				),
+			),
+			array(
+				// Remove duplicate information.
+				array(
+					'address_1' => '815 E St #16 Sacramento CA 95819',
+					'address_2' => '#16',
+					'city'      => 'Sacramento',
+					'state'     => 'CA',
+					'postcode'  => '95819',
+					'country'   => 'US',
+				),
+			),
+			array(
+				// Failing with "no validator"... (anon).
+				// TODO: doesn't correctly remove the duplicatation.
+				array(
+					'address_1' => '560 people\'s plz suite 130',
+					'address_2' => 'Suite 130',
+					'city'      => 'Newark',
+					'state'     => 'DE',
+					'postcode'  => '19702',
 					'country'   => 'US',
 				),
 			),
@@ -84,7 +179,14 @@ class USPS_Address_Validator_Integration_Test extends \Codeception\TestCase\WPTe
 	 */
 	public function test_two_addresses() {
 
-		$usps_username  = $_ENV['USPS_USERNAME'];
+		if ( ! isset( $_ENV['USPS_USERNAME'] ) ) {
+			$this->markTestIncomplete( 'Needs USPS_USERNAME in .env.secret file.' );
+		}
+
+		$usps_username = $_ENV['USPS_USERNAME'];
+
+		$b = 'asd';
+
 		$address_verify = new AddressVerify( $usps_username );
 
 		$address_1 = new Address();
@@ -97,7 +199,7 @@ class USPS_Address_Validator_Integration_Test extends \Codeception\TestCase\WPTe
 
 		$address_2 = new Address();
 		$address_2->setApt( '' );
-		$address_2->setAddress( '1339 63rd St' );
+		$address_2->setAddress( '1312 63rd St' );
 		$address_2->setCity( 'Folsom' );
 		$address_2->setState( 'NV' );
 		$address_2->setZip5( '15819' );
